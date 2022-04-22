4d172447fd456f7bf544082ea029b30812430b08
inx-tapan <tapan.inx@gmail.com>
2022-04-22 16:34:17 +0530

    update: send_notification_user function updated.

--- app/notification/signals.py
+++ app/notification/signals.py
@@ -49,7 +49,7 @@ def send_notification_user(title, data, notification, sent_at, user, from_user):
         for device in devices:
             sm = device.send_message(Message(notification=Notify(title=title, body=data, image=from_user_profile_pic),
                                              data={"from_user": f"{from_user.first_name} {from_user.last_name}",
-                                                   "sent_at": str(sent_at), "notification_id": notification.id}))
+                                                   "sent_at": str(sent_at), "notification_id": str(notification.id)}))
             print(sm)
         return True
     except Exception as e:
--- app/notification/views.py
+++ app/notification/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+import constants
+
 from rest_framework import status, pagination
 from rest_framework.generics import ListAPIView, CreateAPIView
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-import constants
 from notification.models import UserNotification
 
 
