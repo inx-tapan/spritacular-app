027cfdfd214a6e8723769ab7f6a025ac1e4657d2
inx-tapan <tapan.inx@gmail.com>
2022-05-16 13:01:30 +0530

    feat: blog update api added.

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('create_blog/', BlogViewSet.as_view({'post': 'create', 'get': 'retrieve'}), name='blog'),
     # Get Blog details
     path('get_blog/<slug:slug>/', BlogViewSet.as_view({'get': 'retrieve'}), name='get_blog'),
+    # Update Blog/Tutorial
+    path('update_blog/<slug:slug>/', BlogViewSet.as_view({'put': 'update'}), name='update_blog'),
     # Get list of all blogs
     path('get_all_blog/<int:type>/', BlogViewSet.as_view({'get': 'list'}), name='get_all_blog'),
     # Upload Image
